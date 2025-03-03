6a36b2c1d9fd99635c03d35eb939bbb67540f00b
braedan40 <braedan40@boringity.com>
2025-03-03 21:28:54 +0700

    Update and rename games/MainScript.lua to MainScript.lua

---
+++ MainScript.lua
@@ -0,0 +1,5 @@
+local Constlynhub = {
+    [682912579] = 'https://raw.githubusercontent.com/fileagent/Constlynhub/refs/heads/main/games/orderup.lua',
+}
+
+loadstring(game:HttpGet(Constlynhub[game.CreatorId]))()
--- games/MainScript.lua
+++
@@ -1,5 +0,0 @@
-local Constlynhub = {
-    [682912579] = 'https://ronika2.pythonanywhere.com/lemonfunky.lua',
-}
-
-loadstring(game:HttpGet(Constlynhub[game.CreatorId]))()
