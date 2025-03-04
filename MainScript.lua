82629f79d63d63229c51f608aee7c6736ab98bf2
braedan40 <braedan40@boringity.com>
2025-03-05 00:22:22 +0700

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,6 +1,27 @@
+--[[local Constlynhub = {
+    [682912579] = 'https://raw.githubusercontent.com/fileagent/Constlynhub/refs/heads/main/games/orderup.lua',
+    [8918098] = 'https://raw.githubusercontent.com/fileagent/Constlynhub/refs/heads/main/games/notoriety.lua',
+}
+local gamechecker = { -- i add this because the group had diff game
+ [13704594433] = 'https://raw.githubusercontent.com/fileagent/Constlynhub/refs/heads/main/games/the%20storage.lua',
+}
+if not game.PlaceId == gamechecker then
+loadstring(game:HttpGet(Constlynhub[game.CreatorId]))()
+else
+loadstring(game:HttpGet(gamechecker[game.PlaceId]))()]]
 local Constlynhub = {
     [682912579] = 'https://raw.githubusercontent.com/fileagent/Constlynhub/refs/heads/main/games/orderup.lua',
     [8918098] = 'https://raw.githubusercontent.com/fileagent/Constlynhub/refs/heads/main/games/notoriety.lua',
 }
 
-loadstring(game:HttpGet(Constlynhub[game.CreatorId]))()
+local gamechecker = { -- i add this because the group had diff game
+    [13704594433] = 'https://raw.githubusercontent.com/fileagent/Constlynhub/refs/heads/main/games/the%20storage.lua',
+}
+
+if gamechecker[game.PlaceId] then
+    loadstring(game:HttpGet(gamechecker[game.PlaceId]))()
+elseif Constlynhub[game.CreatorId] then
+    loadstring(game:HttpGet(Constlynhub[game.CreatorId]))()
+else
+    warn("ConstlynHub: No script found for this game.")
+end
