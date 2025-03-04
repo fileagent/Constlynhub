06f99b194d07f7431c6e67d31eae373e597f2408
braedan40 <braedan40@boringity.com>
2025-03-04 22:48:53 +0700

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,5 +1,6 @@
 local Constlynhub = {
     [682912579] = 'https://raw.githubusercontent.com/fileagent/Constlynhub/refs/heads/main/games/orderup.lua',
+    [8918098] = 'https://raw.githubusercontent.com/fileagent/Constlynhub/refs/heads/main/games/notoriety.lua',
 }
 
 loadstring(game:HttpGet(Constlynhub[game.CreatorId]))()
